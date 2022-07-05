b5b71aea685a4bfef7a1229927b5eb4d04e7ea43
neo1415 <adedaniel502@gmail.com>
2022-07-05 04:48:26 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-* print_alphabet - Check description
-* Description: It prints the alphabet in lowercase fallowed by a new line
-* Return: Nothing.
+* main - check the code.
+*
+* Return: Always 0.
 */
+
 void print_alphabet(void)
 
 {
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 - Check holberton
-* Description: function uses _putchar function to print
-* alphabet in lowercase 10 times
-* Return: Nothing.
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 
 void print_alphabet_x10(void)
 {
