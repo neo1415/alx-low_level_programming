cb785a275b89af2aa90d97989d4a2409b43d47c0
neo1415 <adedaniel502@gmail.com>
2022-07-05 05:40:54 -0700

    commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - Check Description
+ * @ : input 
+ * Description: function that checks for alphabetic character
+ * Return: 1 if c is a letter, lowercase or uppercase or 0 in otherwise.
+ */
+int _isalpha(int c)
+{
+char lower, upper;
+int isletter = 0;
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+for (upper = 'A'; upper <= 'Z'; upper++)
+{
+if (c == lower || c == upper)
+isletter = 1;
+}
+}
+return (isletter);
+}
